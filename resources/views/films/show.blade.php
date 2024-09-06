2c7b7daf4ec5833a75eddd14065bfab514011121
unknown <oldycherilus@gmail.com>
2024-09-06 13:19:47 +0100

    Ajout du layout au niveau du page show

--- resources/views/films/show.blade.php
+++ resources/views/films/show.blade.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
+@extends('layoutsFilms.home')
+@section('content')
+
     <title>{{ $movie['title'] }}</title>
-</head>
-<body>
+
     <h1>Informations sur le film : {{ $movie->title }}</h1>
     <p> <b>Owerview :</b> {{ $movie->overview}}</p>
     <p> <b>Sortie :</b> {{ $movie->release_date }}</p>
     <p> <b>Vote : </b> {{ $movie->vote_average}}</p>
     <p><b>Poster : </b>{{ $movie->poster_path}}</p>
     <a href="{{ route('films.index') }}">Retour</a>
-</body>
-</html>
+
+@endsection
