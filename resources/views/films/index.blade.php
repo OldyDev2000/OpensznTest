16a54d0961a3fe654f0f3c9f7ff728fcefa9bf44
unknown <oldycherilus@gmail.com>
2024-09-06 13:27:34 +0100

    modification de la vue index

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -1,15 +1,12 @@
 @extends('layoutsFilms.home')
 @section('content')
-
-    <h1>Pages pour lister les films tendances</h1>
-
     <h2>Rechercher un film par son titre :  
         <form action="{route('films.index')}" method="get">
         <input type="text" name="titre">
         <button class="btn border-t-neutral-50 btn-primary">RECHERCHER</button>
     </form>
     </h2>
-    <table class="table table-light">
+    <table class="table table-light table-striped">
         <tr>
             <th>Titre film</th>
             <th>Film Owerview</th>
@@ -21,10 +18,10 @@
             <td>{{ $movie->title }}</td>
             <td>{{ $movie->overview }}</td>
             <td>{{ $movie->release_year}}</td>
-            <td><a href="{{ route('films.show', $movie->id) }}"><button>Details</button></a></td>
+            <td><a href="{{ route('films.show', $movie->id) }}"><button class="btn btn-primary">Details</button></a></td>
             <td>
                 <a href="{{ route('films.destroy', $movie->id) }}">
-                <button onclick="return confirm('Etes vous sure de vouloir supprimer')">Supprimer</button></a>
+                <button class="btn btn-danger" onclick="return confirm('Etes vous sure de vouloir supprimer')">Supprimer</button></a>
             </td>
         </tr>
     @endforeach
