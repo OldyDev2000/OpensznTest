901d711393c3a0271efaeaecde4bee1cb348e55e
unknown <oldycherilus@gmail.com>
2024-09-06 13:11:14 +0100

    Creation de la vue index

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -1,30 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layoutsFilms.home')
+@section('content')
+
     <h1>Pages pour lister les films tendances</h1>
-    <table border="2px">
+
+    <h2>Rechercher un film par son titre :  
+        <form action="{route('films.index')}" method="get">
+        <input type="text" name="titre">
+        <button class="btn border-t-neutral-50 btn-primary">RECHERCHER</button>
+    </form>
+    </h2>
+    <table class="table table-light">
         <tr>
             <th>Titre film</th>
+            <th>Film Owerview</th>
             <th>Date film</th>
             <th colspan="3">Actions</th>
         </tr>
     @foreach ($movies as $movie)
         <tr>
-            <td>{{ $movie['title'] }}</td>
-            <td>{{ $movie['realeasedate'] }}</td>
-            <td><a href="{{ route('films.show', $movie['id']) }}"><button>Details</button></a></td>
-            <td><a href="#"><button>Modifier</button></a></td>
-            <td><a href="#"><button>Supprimer</button></a></td>
+            <td>{{ $movie->title }}</td>
+            <td>{{ $movie->overview }}</td>
+            <td>{{ $movie->release_year}}</td>
+            <td><a href="{{ route('films.show', $movie->id) }}"><button>Details</button></a></td>
+            <td>
+                <a href="{{ route('films.destroy', $movie->id) }}">
+                <button onclick="return confirm('Etes vous sure de vouloir supprimer')">Supprimer</button></a>
+            </td>
         </tr>
     @endforeach
-       
+  
     </table>
  
-
-</body>
-</html>
+@endsection
